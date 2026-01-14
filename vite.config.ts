b3dbe574a430d7a6dde9d4fc3107de3296996568
Nathan Liang <nathan@Nathans-MacBook-Air.local>
2026-01-14 16:06:10 -0500

    corrected base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Replace 'moral-curiosity-approach-avoid' with your exact repository name
-  base: '/moral-curiosity-approach-avoid/', 
+  base: '/moral-curiosity/', 
   build: {
     outDir: 'dist',
   }
