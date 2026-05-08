53b1f28d23a432fcc4d23dcf5e875420f9a4eb1c
Nathan Liang <nathan@Nathans-MacBook-Air.local>
2026-05-08 01:45:11 -0400

    try relative path

--- src/studies/study1b.js
+++ src/studies/study1b.js
@@ -56,8 +56,8 @@ const post_order = pre_order;
 
 // Moral Targets
 const moral_targets = jsPsych.randomization.shuffle([
-  {id: 'good', content: `<img src="../src/stimuli/morally-good-target.svg" alt="Good">`},
-  {id: 'bad', content: `<img src="../src/stimuli/morally-bad-target.svg" alt="Bad">`}
+  {id: 'good', content: `<img src="src/stimuli/morally-good-target.svg" alt="Good">`},
+  {id: 'bad', content: `<img src="src/stimuli/morally-bad-target.svg" alt="Bad">`}
 ]);
 
 // Motives + Motive Labels      
